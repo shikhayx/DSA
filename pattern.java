d1a1327f3d1ed4fc5da5984ca14898e5e425623a
shikhayx <shikhayx@gmail.com>
2026-06-03 04:25:58 +0000

    3 question sovled

---
+++ pattern.java
@@ -0,0 +1,55 @@
+//star pattern
+// class pattern{
+//     public static void main(String args[]){
+//         for(int i=1;i<=4;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//output
+// *
+// **
+// ***
+// ****
+
+//inverted star
+// class pattern{
+//     public static void main(String agrs[]){
+//          int n=4;   //number of rows
+//         for(int i=1;i<=4;i++){
+//             for(int j=1;j<=(n-i+1);j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// output
+// ****
+// ***
+// **
+// *
+
+//half pyramid
+// class pattern{
+//     public static void main(String args[]){
+//         for(int i=1;i<=4;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// output
+// 1
+// 12
+// 123
+// 1234
+
