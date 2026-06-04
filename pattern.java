9d391cf0a93940d21457fe2b54642c17487a1c0d
shikhayx <shikhayx@gmail.com>
2026-06-04 16:15:35 +0000

    11th que

--- pattern.java
+++ pattern.java
@@ -227,3 +227,44 @@
 // ***  ***
 // **    **
 // *      *
+
+
+//solid rhombus pattern
+// class pattern{
+//     public static void main(String args[]){
+//         int n=4;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1;j<=n;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// output
+//    ****
+//   ****
+//  ****
+// ****
+
+//hollow rhombus pattern
+// class pattern{
+//     public static void main(String args[]){
+//         int n=5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n-i;j++){
+//                 if(i+j%2==0){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
