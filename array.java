54475c40533a5b7b61f5591b55af3aa2c706fd72
shikhayx <shikhayx@gmail.com>
2026-06-11 10:50:00 +0000

    pattern completed

--- array.java
+++ array.java
@@ -1,2 +1,56 @@
 //ARRAY 
 //dataType arrayName[] = new dataType[size]
+
+
+//I/O in array
+// import java.util.*;
+// class pattern{
+//     public static void main(String args[]){
+//         int marks[]= new int[50];
+         
+//         Scanner sc = new Scanner(System.in);
+
+//         marks[0]=sc.nextInt();
+//         marks[1]=sc.nextInt();
+//         marks[2]=sc.nextInt();
+
+
+//         System.out.println("phy"+marks[0]+ "chem " + marks[1] +"math" + marks[2]);
+//     }
+// }
+
+//passing array as argument
+//pass by value main no change
+//pass by refernce main change
+
+// class  array{
+//     public static void update(int marks[],int nonchangeable){
+//         nonchangeable=10;
+//         for(int i=0;i<marks.length;i++){
+//             marks[i]=marks[i]+1;
+//         }
+//     }
+//     public static void main(String args[]){
+//         int marks[]= {91,92,93};
+//         int nonchangeable =5;
+//         update(marks,nonchangeable);
+
+//      System.out.println(nonchangeable);
+
+//         for(int i=0;i<marks.length;i++){
+//             System.out.print(marks[i]+" ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+// output
+// 5
+// 92 93 94 
+
+//linear search  in array
+class array{
+    public static void main(String args[]){
+        
+    }
+}
