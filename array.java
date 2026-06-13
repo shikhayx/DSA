2bf615c634afb479e7f5f46ca69bcb432feb667c
shikhayx <shikhayx@gmail.com>
2026-06-13 17:39:58 +0000

    pairs in array

--- array.java
+++ array.java
@@ -49,8 +49,108 @@
 // 92 93 94 
 
 //linear search  in array
-class array{
-    public static void main(String args[]){
-        
-    }
-}
+// class array{
+//     public static int linearSearch(int number[],int key){
+//         for(int i=0;i<number.length;i++){
+//             if(number[i] == key){
+//                 return i;
+//             }
+//         }
+//         return -1;
+
+//     }
+//     public static void main(String args[]){
+//         int number[]={1,2,4,5,6,7};
+//         int key=4;
+//         int index=linearSearch(number,key);
+//         if(index== -1){
+//             System.out.println("element is not found");
+//         }
+//         else{
+//             System.out.println("key is at index:"+index);
+//         }
+
+//     }
+// }
+
+// output
+// key is at index:2
+
+//largest and the smaleest number form the array
+// class array{
+//     public static void main(String args[]){
+//         int number[]={1,2,4,6,7,6};
+//         int largest = Integer.MIN_VALUE;
+//         int smallest=Integer.MAX_VALUE;
+
+//         for(int i=0;i<number.length;i++){
+//             if(largest<number[i]){
+//                 largest=number[i];
+//             }
+//             if(smallest>number[i]){
+//                 smallest=number[i];
+//             }
+//         }
+//         System.out.println("largest element from the array:"+largest);
+//         System.out.println("smallest element from the array:"+smallest);
+//     }
+// }
+// output
+// largest element from the array:7
+// smallest element from the array:1
+
+
+//binary search-prerequisite=sorted array
+// import java.util.*;
+
+// class Main {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 3, 5, 7, 9};
+//         int target = 7;
+
+//         int index = Arrays.binarySearch(arr, target);
+//         System.out.println(index);
+//     }
+// }
+
+//reverse number
+// class array{
+//     public static void main(String args[]){
+//         int number[]={1,2,5,7,8};
+//         int start =0,last=number.length-1;
+//         while(start<last){
+//             int temp=number[last];
+//             number[last]=number[start];
+//             number[start]= temp;
+//             start++;
+//             last--;
+//         }
+//         for(int i=0;i<number.length;i++){
+//             System.out.print(number[i]+" ");
+//         }
+//         System.out.println();
+//     }
+// }
+// output
+// 8 7 5 2 1 
+
+//pairs in an array
+// class array{
+//     public static void main(String args[]){
+//         int number[]={1,2,3,4,5};
+
+//         for(int i=0;i<number.length;i++){
+//             int curr = number[i];
+//             for(int j= i+1;j<number.length;j++){
+//                 System.out.print("(" +curr +","+number[j]+")");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// output
+// (1,2)(1,3)(1,4)(1,5)
+// (2,3)(2,4)(2,5)
+// (3,4)(3,5)
+// (4,5)
